52bd8896ec01355e76b78e627d865b46bfea8303
FdoLamarca <39487084+FdoLamarca@users.noreply.github.com>
2020-01-06 21:01:58 +0100

    21:01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Exevi';
+  title = '¡ Bienvenido al Indice de Equilibrio !';
 }
